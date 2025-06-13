9132156c330231813a9c44a69483a015dffc53c5
wahyuhadi <hrahmatwahyu@gmail.com>
2025-06-13 20:44:41 +0800

    make beuti

--- apps.go
+++ apps.go
@@ -33,14 +33,26 @@ func ishost(stdin string) string {
 func main() {
 	// JARM	192.168.2.1:443	29d14d00029d29d00029d14d29d29df26cd45df722aff084b72395f8d18d1b
 	// format JARM IP HASH
+	var total_found = 0
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
+		gologger.Info().Msg("Start Finding The RAT....")
+
 		ishash := ishash(sc.Text())
 		ishost := ishost(sc.Text())
 		msg := fmt.Sprintf("Host %s", ishost)
 		isfind, itype := findIndex(ishash)
 		if isfind {
-			gologger.Info().Str("Fingerprit", ishash).Str("Found RAT", *itype).Msg(msg)
+			total_found += 1
+			gologger.Info().Str("Fingerprint", ishash).Str("Found RAT", *itype).Msg(msg)
 		}
 	}
+
+	if total_found == 0 {
+		gologger.Info().Msg("[Done] No RAT Found")
+	} else {
+		gologger.Info().Msg(fmt.Sprintf("[Done] Total Found RAT %v", total_found))
+
+	}
+
 }
